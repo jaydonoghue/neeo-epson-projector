9106b4837c163a3411a0d824e6dea95852d6d4da
jaydonoghue <jay@donoghue.org>
2017-06-15 21:19:48 -0400

    Initial commit - ON/OFF functionality

---
+++ controller.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var net = require('net');
+
+// Set your IP here
+var PJ_IP = '10.11.12.50';
+var PJ_PORT = '4098'
+
+/* Matrix Controller */
+
+module.exports.onButtonPressed = function onButtonPressed(name) {
+    console.log(`[EPSON CONTROLLER] ${name} button pressed`);
+
+
+    var client = new net.Socket();
+    client.connect(PJ_PORT, PJ_IP, function () {
+        console.log('Connected');
+
+        // We only deal with power right now
+        var stringToSend = "PWR " + name.toUpperCase() + "\r\n";
+
+        console.log('Sent Message %s', stringToSend);
+
+        client.write(stringToSend);
+
+
+        client.destroy();
+    });
+
+
+};
+
+
---
+++ index.js
@@ -0,0 +1,56 @@
+'use strict';
+
+const neeoapi = require('neeo-sdk');
+const controller = require('./controller');
+
+console.log('Epson TCP Projector');
+console.log('---------------------------------------------');
+
+/*
+ * Adapter - an Adapter to connect inputs + outputs from HDBaseT matrix
+ */
+
+// first we set the device info, used to identify it on the Brain
+const pj = neeoapi.buildDevice('Epson TCP Projector')
+    .setManufacturer('Epson')
+    .addAdditionalSearchToken('Projector')
+    .addAdditionalSearchToken('8350UB')
+    .setType("ACCESSOIRE")
+    //.setType('Matrix')
+
+    // Capabilities of the device
+    .addButton({ name: 'on', label: 'Power On' })
+    .addButton({ name: 'off', label: 'Power Off' })
+    
+    .addButtonHander(controller.onButtonPressed);
+
+function startSdkExample(brain) {
+    console.log('- Start server');
+    neeoapi.startServer({
+        brain,
+        port: 6336,
+        name: 'epson-projector',
+        devices: [pj]
+    })
+        .then(() => {
+            console.log('# READY! use the NEEO app to search for "Epson TCP".');
+        })
+        .catch((error) => {
+            //if there was any error, print message out to console
+            console.error('ERROR!', error.message);
+            process.exit(1);
+        });
+}
+
+const brainIp = process.env.BRAINIP;
+if (brainIp) {
+    console.log('- use NEEO Brain IP from env variable', brainIp);
+    startSdkExample(brainIp);
+} else {
+    console.log('- discover one NEEO Brain...');
+    neeoapi.discoverOneBrain()
+        .then((brain) => {
+            console.log('- Brain discovered:', brain.name);
+            startSdkExample(brain);
+        });
+}
