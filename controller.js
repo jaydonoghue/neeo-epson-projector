9fbc10af92ee246f305d4571c65dafc6af60df38
jaydonoghue <jay@donoghue.org>
2017-07-04 18:26:03 -0400

    Add CMODE commands for brightness

--- controller.js
+++ controller.js
@@ -4,26 +4,29 @@ var net = require('net');
 
 // Set your IP here
 var PJ_IP = '10.11.12.50';
-var PJ_PORT = '4098'
+var PJ_PORT = '4098';
 
 /* Matrix Controller */
 
 module.exports.onButtonPressed = function onButtonPressed(name) {
     console.log(`[EPSON CONTROLLER] ${name} button pressed`);
 
-
+    var stringToSend = "";
     var client = new net.Socket();
     client.connect(PJ_PORT, PJ_IP, function () {
         console.log('Connected');
 
         if (name == "on" || name == "off") {
-            var stringToSend = "PWR " + name.toUpperCase() + "\r\n";
-
-            console.log('Sent Message %s', stringToSend);
-
-            client.write(stringToSend);
+            stringToSend = "PWR " + name.toUpperCase();
+        } else if (name.substring(0, 5) == "CMODE") {
+            stringToSend = name;
         }
 
+        if (stringToSend != '') {
+            console.log('Sent Message %s', stringToSend);
+
+            client.write(stringToSend + "\r\n");
+        }
         client.destroy();
     });
 
